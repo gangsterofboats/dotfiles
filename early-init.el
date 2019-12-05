08bfeca55c9b6b5a1de470e74014f50ef7c7640e
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2019-12-05 10:38:45 -0800

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -1,19 +1,21 @@
 ;;;; -*- lexical-binding: t -*-
-;;;; Emacs configuration -- early-init.el file
-;;;; Copyright (C) 2019 Michael Wiseman
-;;;; 
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as
-;;;; published by the Free Software Foundation, either version 3 of the
-;;;; License, or (at your option) any later version.
-;;;; 
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;; 
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emacs configuration -- early-init.el file                                ;;
+;; Copyright (C) 2019 Michael Wiseman                                       ;;
+;;                                                                          ;;
+;; This program is free software: you can redistribute it and/or modify     ;;
+;; it under the terms of the GNU Affero General Public License as           ;;
+;; published by the Free Software Foundation, either version 3 of the       ;;
+;; License, or (at your option) any later version.                          ;;
+;;                                                                          ;;
+;; This program is distributed in the hope that it will be useful,          ;;
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of           ;;
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ;;
+;; GNU Affero General Public License for more details.                      ;;
+;;                                                                          ;;
+;; You should have received a copy of the GNU Affero General Public License ;;
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; FIX for something
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
