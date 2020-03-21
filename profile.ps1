31356a9e1b591ed3b44574cf1eff90a4fd6cf545
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2020-03-20 17:23:51 -0700

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -7,11 +7,14 @@ function Prompt
     # A copy of fish shell's nim prompt
     # The original one is by Guilhem "Nim" Saurel − https://github.com/nim65s/dotfiles/
 
+    # This part taken from https://gist.github.com/JTBrinkmann/f86d52fe8dd8e76331fa4050e5fcbcbc
+    $prompt_pwd = (Get-Location).Path.Replace("$ENV:HOME", "~") -Replace "(\.?[^/]{1})[^/]*/", '$1/'
+
     Write-Host "┬─[" -foregroundColor Green -NoNewline
     Write-Host "$(whoami)" -foregroundColor Yellow -NoNewline
     Write-Host "@" -foregroundColor White -NoNewline
     Write-Host "$(hostname)" -foregroundColor Blue -NoNewline
-    Write-Host ":$(Split-Path -Leaf -Path (Get-Location))" -foregroundColor White -NoNewline
+    Write-Host ":$prompt_pwd" -foregroundColor White -NoNewline
     Write-Host "]─[" -foregroundColor Green -NoNewline
     Write-Host "$(Get-Date -Format `"hh:mm:ss tt`")" -foregroundColor DarkGreen -NoNewline
     Write-Host "]" -foregroundColor Green
