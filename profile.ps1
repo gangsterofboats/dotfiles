ed95baacb0fbe24775893c94b03f1ca6234ee415
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2020-03-20 19:17:02 -0700

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,13 +1,15 @@
+#### Staring banner
 Clear-Host
 fortune -ace
 Write-Host ""
 
+#### Prompt setting
+### A copy of fish shell's nim prompt
+### The original one is by Guilhem "Nim" Saurel − https://github.com/nim65s/dotfiles/
+
 function Prompt
 {
-    # A copy of fish shell's nim prompt
-    # The original one is by Guilhem "Nim" Saurel − https://github.com/nim65s/dotfiles/
-
-    # This part taken from https://gist.github.com/JTBrinkmann/f86d52fe8dd8e76331fa4050e5fcbcbc
+    ## This part taken from https://gist.github.com/JTBrinkmann/f86d52fe8dd8e76331fa4050e5fcbcbc
     $prompt_pwd = (Get-Location).Path.Replace("$ENV:HOME", "~") -Replace "(\.?[^/]{1})[^/]*/", '$1/'
 
     Write-Host "┬─[" -foregroundColor Green -NoNewline
@@ -23,3 +25,6 @@ function Prompt
 
     Return " "
 }
+
+#### Other settings
+$MaximumHistoryCount = 32768
