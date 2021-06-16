3fe775ac28e3e2fc8a957415980f7333017a6008
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2021-06-15 17:39:27 -0700

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,6 @@
 ##############################################################################
 ## Powershell configuration -- profile.ps1 file                             ##
-## Copyright (C) 2020 Michael Wiseman                                       ##
+## Copyright (C) 2021 Michael Wiseman                                       ##
 ##                                                                          ##
 ## This program is free software: you can redistribute it and/or modify it  ##
 ## under the terms of the GNU Affero General Public License as published by ##
@@ -18,31 +18,11 @@
 
 #### Staring banner
 # Clear-Host
-fortune -ace
+wsl fortune -ace
 Write-Host ""
 
 #### Prompt setting
-### A copy of fish shell's nim prompt
-### The original one is by Guilhem "Nim" Saurel − https://github.com/nim65s/dotfiles/
-
-function Prompt
-{
-    ## This part taken from https://gist.github.com/JTBrinkmann/f86d52fe8dd8e76331fa4050e5fcbcbc
-    $prompt_pwd = (Get-Location).Path.Replace("$ENV:HOME", "~") -Replace "(\.?[^/]{1})[^/]*/", '$1/'
-
-    Write-Host "┬─[" -foregroundColor Green -NoNewline
-    Write-Host "$(whoami)" -foregroundColor Yellow -NoNewline
-    Write-Host "@" -foregroundColor White -NoNewline
-    Write-Host "$(hostname)" -foregroundColor Blue -NoNewline
-    Write-Host ":$prompt_pwd" -foregroundColor White -NoNewline
-    Write-Host "]─[" -foregroundColor Green -NoNewline
-    Write-Host "$(Get-Date -Format `"hh:mm:ss tt`")" -foregroundColor DarkGreen -NoNewline
-    Write-Host "]" -foregroundColor Green
-    Write-Host "╰─>" -foregroundColor Green -NoNewLine
-    Write-Host "$" -foregroundColor Red -NoNewLine
-
-    Return " "
-}
+Invoke-Expression (&starship init powershell)
 
 #### PSReadLine Settings
 (Get-PSReadLineOption).HistorySearchCaseSensitive = $True
