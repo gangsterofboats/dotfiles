a0ffe33af0f0b37331ea0aedf497259bba35b5de
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2021-07-11 17:23:55 -0700

    Add files via upload

---
+++ ginit.lua
@@ -0,0 +1,28 @@
+------------------------------------------------------------------------------
+-- Neovim configuration -- ginit.vim file                                   --
+-- Copyright (C) 2019 Michael Wiseman                                       --
+--                                                                          --
+-- This program is free software: you can redistribute it and/or modify     --
+-- it under the terms of the GNU Affero General Public License as           --
+-- published by the Free Software Foundation, either version 3 of the       --
+-- License, or (at your option) any later version.                          --
+--                                                                          --
+-- This program is distributed in the hope that it will be useful,          --
+-- but WITHOUT ANY WARRANTY- without even the implied warranty of           --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            --
+-- GNU Affero General Public License for more details.                      --
+--                                                                          --
+-- You should have received a copy of the GNU Affero General Public License --
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.   --
+------------------------------------------------------------------------------
+
+--- General settings
+vim.fn.colorscheme('moonfly')
+vim.opt.guifont = 'Fira Code:h12'
+vim.opt.lines = 30
+vim.opt.columns = 100
+vim.opt.title = true
+vim.opt.titlestring = '%t\ -\ Neovim'
+
+--- Make shift-insert work like in Xterm
+vim.api.nvim_set_keymap('', '<S-Insert>', '<MiddleMouse>')
---
+++ init.lua
@@ -0,0 +1,103 @@
+------------------------------------------------------------------------------
+-- Neovim configuration -- init.vim file                                    --
+-- Copyright (C) 2021 Michael Wiseman                                       --
+--                                                                          --
+-- This program is free software: you can redistribute it and/or modify it  --
+-- under the terms of the GNU Affero General Public License as published by --
+-- the Free Software Foundation, either version 3 of the License, or (at    --
+-- your option) any later version.                                          --
+--                                                                          --
+-- This program is distributed in the hope that it will be useful, but      --
+-- WITHOUT ANY WARRANTY" without even the implied warranty of               --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero --
+-- General Public License for more details.                                 --
+--                                                                          --
+-- You should have received a copy of the GNU Affero General Public License --
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.   --
+------------------------------------------------------------------------------
+
+---- No Vi/Vim compatibility (must be set first)
+vim.opt.cpoptions = ''
+
+---- Settings
+
+--- Backup file settings
+-- set backupdir-=.
+vim.cmd([[autocmd BufWritePre * let &backupext = substitute(expand('%:p:h'), '/', '%', 'g')]])
+vim.opt.backup = true
+vim.opt.undofile = true
+local backupdir = vim.fn.expand(vim.fn.stdpath('data') .. '/backup//')
+vim.opt.backupdir = backupdir
+-- vim.opt.writebackup = true
+
+--- Enable command-line completion
+vim.opt.wildmode = 'list:longest,full'
+-- vim.opt.wildmode = {'list', 'longest', 'full'}
+
+--- Enable visual selection using the mouse in terminals
+vim.opt.mouse = 'a'
+
+--- File settings
+vim.opt.fileformat = 'unix'
+vim.opt.fileformats = 'unix,dos,mac'
+
+--- Indent settings
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+--- Keymaps
+vim.g.mapleader = '\\'
+vim.api.nvim_set_keymap('', 'gf', ':e <cfile><CR>')
+vim.api.nvim_set_keymap('', 'Y', 'y$')
+vim.api.nvim_set_keymap('i', '<C-U>', '<C-G>u<C-U>', { noremap = true })
+
+--- Move directory to match current file
+vim.opt.autochdir = true
+vim.opt.browsedir = 'buffer'
+
+--- Netrw settings
+let g:netrw_altv          = 1
+let g:netrw_fastbrowse    = 2
+let g:netrw_keepdir       = 0
+let g:netrw_liststyle     = 2
+let g:netrw_retmap        = 1
+let g:netrw_silent        = 1
+let g:netrw_special_syntax= 1
+
+--- Search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+--- Set line numbers
+vim.opt.number = true
+
+--- Other settings
+let c_comment_strings=1
+packadd! matchit
+-- packadd! shellmenu
+packadd! vimball
+vim.opt.cinoptions=>s,e0,n0,f0,{0,}4,^-1s,:0,=s,g0,h1s,p2,t0,+2,(2,)20,*30
+vim.opt.complete+=i
+vim.opt.display=truncate
+vim.opt.fillchars = { vert: '│', fold: '·' }
+vim.opt.hidden = true
+vim.opt.inccommand=nosplit
+vim.opt.listchars = { tab: '>\\', trail: '-', extends:'>', precedes:'<', nbsp: '+' }
+vim.opt.nostartofline = true
+vim.opt.nrformats = { 'bin', 'hex', 'octal', 'alpha' }
+vim.opt.scrolloff = 5
+vim.opt.showmatch = true
+vim.opt.sidescrolloff = 5
+vim.opt.termguicolors = true
+vim.opt.textwidth = 80
+vim.opt.ttimeoutlen = 100
+
+---- Functions
+
+--- Diff current loaded buffer and the originating file
+-- if !exists(':DiffOrig'
+  -- command DiffOrig vert new | set buftype=nofile | read ++edit # | 0d_ | diffthis | wincmd p | diffthis
+-- endif
