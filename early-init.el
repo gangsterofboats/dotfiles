9895f436a6718be9387878d34eaf9fb4c1437f9e
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2021-11-26 11:38:39 -0800

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -21,13 +21,11 @@
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/devel/")              ; GNU
         ("nongnu" . "https://elpa.nongnu.org/nongnu-devel/") ; NonGNU
-        ("melpa" . "https://melpa.org/packages/")            ; MELPA
-        ("org" . "https://orgmode.org/elpa/")))              ; Org Mode
+        ("melpa" . "https://melpa.org/packages/")))          ; MELPA
 
 ;;; Package archives order
 (setq package-archive-priorities
       '(("melpa" . 20)
-        ("org" . 20)
         ("gnu" . 10)
         ("nongnu" . 10)))
 
